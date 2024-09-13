b7a80c2144801e4a8d6189767ef7bb28ab9348fd
aaron6yi <aaron6yi@gmail.com>
2024-09-14 03:04:45 +0800

    update homework2 again

--- 02_activities/homework/homework_2.sql
+++ 02_activities/homework/homework_2.sql
@@ -76,9 +76,10 @@ FROM product;
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
-SELECT v.*, vba.* 
-FROM vendor v
-INNER JOIN vendor_booth_assignments vba 
-ON v.vendor_id = vba.vendor_id
-ORDER BY v.vendor_name, vba.market_date;
+
+SELECT vendor.*, vendor_booth_assignments.* 
+FROM vendor
+INNER JOIN vendor_booth_assignments 
+    ON vendor.vendor_id = vendor_booth_assignments.vendor_id
+ORDER BY vendor.vendor_name, vendor_booth_assignments.market_date;
 
